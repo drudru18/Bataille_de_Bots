894b7842ffb71765c2c3cba78a6df89cbe24fe97
drudru18 <alexandru.doroiman@student.uclouvain.be>
2022-07-14 22:56:17 +0200

    Version 1.2, ajout de nouvelles fonctionnalités

--- Bataille entre 2 bots.py
+++ Bataille entre 2 bots.py
@@ -8,11 +8,16 @@ import random
 class Player:
     def __init__(self, name):
         self.name = name
-        Battle(100, 100, 0, 0, self.name)
+        Battle(100, 100, 0, 0, 0, 0, 0, 0, self.name)
         
 
 class Battle(Player):
-    def __init__(self, life1, life2, combo1, combo2, name):
+    def __init__(self, life1, life2, combo1, combo2, win1, win2, lose1, lose2, name):
+        self.start = time.time()
+        self.lose1 = lose1
+        self.lose2 = lose2
+        self.win1 = win1
+        self.win2 = win2
         self.name = name
         self.combo1 = combo1
         self.combo2 = combo2
@@ -28,13 +33,16 @@ class Battle(Player):
             if self.random == 1:
                 self.bot1()
             self.bot2()
-        ou = str(input(30*"\n"+'\033[0m'+"Voulez-vous recommencer un combat sous le pseudo "+'\033[1m'+self.name+'\033[0m'+" ? OUI/NON \n -->  "+'\033[1m'))
-        if ou.lower() == "oui":
-            Battle(100, 100, 0, 0, self.name)
-        elif ou.lower() == "non":
+        self.ou = str(input(30*"\n"+'\033[0m'+"Voulez-vous recommencer un combat sous le pseudo "+'\033[1m'+self.name.upper()+'\033[0m'+" ? OUI/NON \n -->  "+'\033[1m'))
+        if self.ou.lower() == "oui":
+            Battle(100, 100, 0, 0, self.win1, self.win2, self.lose1, self.lose2, self.name)
+        elif self.ou.lower() == "non":
+            print("\nCe programme se ferme")
+            time.sleep(0.5)
             exit()
         else:
-            print("Coisissez oui ou non !")
+            print("Coisissez oui ou non ! (ce programme se ferme)")
+            time.sleep(0.5)
             exit()
 
     def bot1(self):
@@ -58,7 +66,7 @@ class Battle(Player):
                 self.life2-=self.damage
                 time.sleep(random.randint(1, 2))
                 print("Le BOT 1 a contre-attaqué le BOT 2 et lui a mis "+ str(self.damage) + " dégâts !\n")
-        return Battle(self.life1, self.life2, self.combo1, self.combo2, self.name)
+        return Battle(self.life1, self.life2, self.combo1, self.combo2, self.win1, self.win2, self.lose1, self.lose2, self.name)
     
     def bot2(self):
         self.combo1 = 0
@@ -81,16 +89,28 @@ class Battle(Player):
                 self.life1-=self.damage
                 time.sleep(random.randint(1, 2))
                 print("Le BOT 2 a contre-attaqué le BOT 1 et lui a mis "+ str(self.damage) + " dégâts !\n")
-        return Battle(self.life1, self.life2, self.combo1, self.combo2, self.name)
+        return Battle(self.life1, self.life2, self.combo1, self.combo2, self.win1, self.win2, self.lose1, self.lose2, self.name)
 
     def attack(self):
         self.damage = random.randint(8, 12)
     
+    def leaderboard(self):
+        with open("log.txt", "a") as file:
+            
+            file.write("Username:\t"+self.name.upper()+"\t\t\t\tTemps passé: "+str(round(self.total_time, 2))+" sec.\n\nBot_1\t\tCombats Gagnés:\t"+str(self.win1)+"\t\tCombats Perdus:\t"+str(self.lose1)+"\nBot_2\t\tCombats Gagnés:\t"+str(self.win2)+"\t\tCombats Perdus:\t"+str(self.lose2)+"\n\n----------------------------------------------------------------------\n\n")
+        
     def __str__(self):
         if self.life1 > self.life2:
             print(Fore.LIGHTRED_EX+'\033[1m'+"Le BOT 1 a gagné !"+Fore.WHITE)
+            self.win1+=1
+            self.lose2+=1
         else:
             print(Fore.LIGHTRED_EX+'\033[1m'+"Le BOT 2 a gagné !"+Fore.WHITE)
+            self.win2+=1
+            self.lose1+=1
+        self.end = time.time()
+        self.total_time = self.end - self.start
+        self.leaderboard()
         time.sleep(3)
     
-Player(str(input("Please enter your name: ")))
+Player(str(input("Veuillez entrer votre nom d'utilisateur: ")))
